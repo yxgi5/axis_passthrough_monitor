4d93d941624ea6287f296b43fea6878c5fd60c6c
Andreas Zhang <denglitsch@gmail.com>
2022-03-24 09:41:37 +0800

    feat: add a axi-lite interface and defined registers which can be read through axi-lite bus

--- hdl/axis_passthrough_mon.v
+++ hdl/axis_passthrough_mon.v
@@ -4,7 +4,10 @@ module axis_passthrough_mon
       (
        parameter WIDTH = 32'd48,
        parameter TUSER_WIDTH = 32'd1,
-       parameter FREQ_HZ = 32'd100000000
+       parameter FREQ_HZ = 32'd100000000, 
+        // Parameters of Axi Slave Bus Interface S00_AXI
+		parameter integer C_S00_AXI_DATA_WIDTH	= 32,
+		parameter integer C_S00_AXI_ADDR_WIDTH	= 7
       )
      (
     input aclk,
@@ -20,8 +23,54 @@ module axis_passthrough_mon
     (* DONT_TOUCH = "yes", s="true",keep="true" *)input m_axis_tready,
     (* DONT_TOUCH = "yes", s="true",keep="true" *)output [WIDTH-1:0] m_axis_tdata,
     (* DONT_TOUCH = "yes", s="true",keep="true" *)output m_axis_tlast,
-    (* DONT_TOUCH = "yes", s="true",keep="true" *)output [TUSER_WIDTH-1:0]m_axis_tuser
+    (* DONT_TOUCH = "yes", s="true",keep="true" *)output [TUSER_WIDTH-1:0]m_axis_tuser,
+
+    	// Ports of Axi Slave Bus Interface S00_AXI
+		input s00_axi_aclk,
+		input s00_axi_aresetn,
+		input [C_S00_AXI_ADDR_WIDTH-1 : 0] s00_axi_awaddr,
+		input [2 : 0] s00_axi_awprot,
+		input s00_axi_awvalid,
+		output s00_axi_awready,
+		input [C_S00_AXI_DATA_WIDTH-1 : 0] s00_axi_wdata,
+		input [(C_S00_AXI_DATA_WIDTH/8)-1 : 0] s00_axi_wstrb,
+		input s00_axi_wvalid,
+		output s00_axi_wready,
+		output [1 : 0] s00_axi_bresp,
+		output s00_axi_bvalid,
+		input s00_axi_bready,
+		input [C_S00_AXI_ADDR_WIDTH-1 : 0] s00_axi_araddr,
+		input [2 : 0] s00_axi_arprot,
+		input s00_axi_arvalid,
+		output s00_axi_arready,
+		output [C_S00_AXI_DATA_WIDTH-1 : 0] s00_axi_rdata,
+		output [1 : 0] s00_axi_rresp,
+		output s00_axi_rvalid,
+		input s00_axi_rready
     );
+
+wire s00_axi_aclk;
+wire s00_axi_aresetn;
+wire [C_S00_AXI_ADDR_WIDTH-1 : 0] s00_axi_awaddr;
+wire [2 : 0] s00_axi_awprot;
+wire s00_axi_awvalid;
+wire s00_axi_awready;
+wire [C_S00_AXI_DATA_WIDTH-1 : 0] s00_axi_wdata;
+wire [(C_S00_AXI_DATA_WIDTH/8)-1 : 0] s00_axi_wstrb;
+wire s00_axi_wvalid;
+wire s00_axi_wready;
+wire [1 : 0] s00_axi_bresp;
+wire s00_axi_bvalid;
+wire s00_axi_bready;
+wire [C_S00_AXI_ADDR_WIDTH-1 : 0] s00_axi_araddr;
+wire [2 : 0] s00_axi_arprot;
+wire s00_axi_arvalid;
+wire s00_axi_arready;
+wire [C_S00_AXI_DATA_WIDTH-1 : 0] s00_axi_rdata;
+wire [1 : 0] s00_axi_rresp;
+wire s00_axi_rvalid;
+wire s00_axi_rready;
+
 //
 assign m_axis_tvalid = s_axis_tvalid;
 assign s_axis_tready = m_axis_tready;
@@ -134,4 +183,35 @@ begin
     end
 end
 ////////////////
+
+// Instantiation of Axi Bus Interface S00_AXI
+	AXI_LITE_REG_v1_0_S00_AXI # ( 
+		.C_S_AXI_DATA_WIDTH(C_S00_AXI_DATA_WIDTH),
+		.C_S_AXI_ADDR_WIDTH(C_S00_AXI_ADDR_WIDTH)
+	) AXI_LITE_REG_v1_0_S00_AXI_inst (
+	    .col(col),
+        .line(line),
+        .fps(fps),
+		.S_AXI_ACLK(s00_axi_aclk),
+		.S_AXI_ARESETN(s00_axi_aresetn),
+		.S_AXI_AWADDR(s00_axi_awaddr),
+		.S_AXI_AWPROT(s00_axi_awprot),
+		.S_AXI_AWVALID(s00_axi_awvalid),
+		.S_AXI_AWREADY(s00_axi_awready),
+		.S_AXI_WDATA(s00_axi_wdata),
+		.S_AXI_WSTRB(s00_axi_wstrb),
+		.S_AXI_WVALID(s00_axi_wvalid),
+		.S_AXI_WREADY(s00_axi_wready),
+		.S_AXI_BRESP(s00_axi_bresp),
+		.S_AXI_BVALID(s00_axi_bvalid),
+		.S_AXI_BREADY(s00_axi_bready),
+		.S_AXI_ARADDR(s00_axi_araddr),
+		.S_AXI_ARPROT(s00_axi_arprot),
+		.S_AXI_ARVALID(s00_axi_arvalid),
+		.S_AXI_ARREADY(s00_axi_arready),
+		.S_AXI_RDATA(s00_axi_rdata),
+		.S_AXI_RRESP(s00_axi_rresp),
+		.S_AXI_RVALID(s00_axi_rvalid),
+		.S_AXI_RREADY(s00_axi_rready)
+	);
 endmodule
